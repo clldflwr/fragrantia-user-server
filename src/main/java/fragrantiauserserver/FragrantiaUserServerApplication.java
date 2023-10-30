231d724405e8e10a242f44a5b1b99148e84ae302
정익 <dlwjddlr97@gmail.com>
2023-10-30 17:55:28 +0900

    Initial commit

---
+++ src/main/java/fragrantiauserserver/FragrantiaUserServerApplication.java
@@ -0,0 +1,13 @@
+package fragrantiauserserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FragrantiaUserServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FragrantiaUserServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fragrantiauserserver/fragrantiauser/fragrantiauserserver/FragrantiaUserServerApplicationTests.java
@@ -0,0 +1,13 @@
+package fragrantiauserserver.fragrantiauser.fragrantiauserserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FragrantiaUserServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
